7f5fa5b5254a3877be91c65ce056bfdb0704d6a8
ozcansayis <ozcansayis@gmail.com>
2024-03-09 00:29:44 +0300

    BaseDriver updated.

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -2,6 +2,7 @@ package Utility;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -9,11 +10,13 @@ import java.time.Duration;
 
 public class BaseDriver {
     public static WebDriver driver;
+    public static WebDriver driverF;
     public static WebDriverWait wait;
     public static Actions dAct;
 
     static {
         driver = new ChromeDriver();
+        driverF = new FirefoxDriver();
         dAct = new Actions(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
